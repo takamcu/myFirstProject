b072ebe948681809bb0deaf572b560543abb9b74
takamcu <takayuki.mcu@outlook.jp>
2022-10-26 14:15:12 +0900

    test for Japanese text

--- main.py
+++ main.py
@@ -6,12 +6,12 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}!!')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = input("What is your name?")
+    name = input("あなたのお名前は？")
     print_hi(name)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
